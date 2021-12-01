7d5d82007127d0e294702e784a083acc5fb2cba3
Aftab-1998 <alam41976@gmail.com>
2021-12-01 16:06:46 +0530

    [Aftab1] Add ; Added flipcoin twist

---
+++ BasicCoreProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BasicCoreProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. Exit");
+            
+            int option = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            while (flag)
+            {
+                switch (option)
+                {
+                    case 1:
+                        BasicPrograms programs = new BasicPrograms();
+                        programs.FlipCoin();
+                        break;
+                        case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
